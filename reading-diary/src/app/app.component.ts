b9ab19a732965794ce84c21d3bad57ca841714af
Maya Peneva <m.peneva@onebitsoftware.net>
2025-03-06 22:13:48 +0200

    Added app module. Added some content in app component. Made changes in navigation component.

--- reading-diary/src/app/app.component.ts
+++ reading-diary/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./navigation/navigation.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [NavigationComponent]
 })
+
 export class AppComponent {
   title = 'reading-diary';
 }
+ 
