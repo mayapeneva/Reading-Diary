cb7e2d113bce1036cd6f54b4c9b3e05fcc786069
Maya Peneva <m.peneva@onebitsoftware.net>
2025-03-14 22:55:05 +0200

    Removed app module, as we do not need such with standalone components.

--- reading-diary/src/app/app.component.ts
+++ reading-diary/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from "./navigation/navigation.component";
+import { HttpClient } from '@angular/common/http';
+import { BooksService } from './service/books.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [NavigationComponent]
+  imports: [ NavigationComponent],
+  providers: [ HttpClient, BooksService ]
 })
 
 export class AppComponent {
-  title = 'reading-diary';
 }
  
