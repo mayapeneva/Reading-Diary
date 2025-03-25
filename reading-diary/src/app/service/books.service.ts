e3c22d9139cf798b48c38b677c7056912645c224
Maya Peneva <m.peneva@onebitsoftware.net>
2025-03-25 19:13:19 +0200

    Added more find books methods in booksService.

--- reading-diary/src/app/service/books.service.ts
+++ reading-diary/src/app/service/books.service.ts
@@ -2,21 +2,41 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Books } from '../models/books.model';
+import { environment } from '../../environments/environment.development';
 
 const path = 'https://www.googleapis.com/books/v1/volumes';
-const isbn = 'isbn'
 const authentication = 'key';
 
 @Injectable({
   providedIn: 'root'
 })
-
+ 
 export class BooksService {
 
   constructor(private httpClient: HttpClient) { }
 
-  findBookByIsbn(searchTerm: string): Observable<Books> {
-    const queryString = `${path}?q=${isbn}:${searchTerm}&${authentication}=${GoogleApiApiKey}`;
+  findBookByIsbn(isbn: string): Observable<Books> {
+    const queryString = `${path}?q=isbn:${isbn}&${authentication}=${environment.googleApi_ApiKey}`;
     return this.httpClient.get<Books>(queryString);
   }
+
+  findBookByTitle(title: string): Observable<Books> {
+    const queryString = `${path}?q=intitle:${title}&${authentication}=${environment.googleApi_ApiKey}`;
+    return this.httpClient.get<Books>(queryString);
+  }
+
+  findBookByAuthor(author: string): Observable<Books> {
+    const queryString = `${path}?q=inauthor:${author}+orderBy=newest&${authentication}=${environment.googleApi_ApiKey}`;
+    return this.httpClient.get<Books>(queryString);
+  }
+
+  findBookByPublisher(publisher: string): Observable<Books> {
+    const queryString = `${path}?q=inpublisher:${publisher}&${authentication}=${environment.googleApi_ApiKey}`;
+    return this.httpClient.get<Books>(queryString);
+  }
+
+  findBookByCategory(category: string): Observable<Books> {
+    const queryString = `${path}?q=subject:${category}&orderBy=newest&${authentication}=${environment.googleApi_ApiKey}`;
+    return this.httpClient.get<Books>(queryString);
+  } 
 }
