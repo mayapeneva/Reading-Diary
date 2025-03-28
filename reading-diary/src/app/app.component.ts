7c1c3d9a0ee8fef3655a9f85664b1124c4adf3da
Maya Peneva <m.peneva@onebitsoftware.net>
2025-03-28 11:23:16 +0200

    Cleared the app component.

--- reading-diary/src/app/app.component.ts
+++ reading-diary/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { OpenLibraryBooksService } from './service/openlibrary.books.service';
-import { GoogleApiBooksService } from './service/googleapi.books.service';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { RouterOutlet } from '@angular/router';
 
@@ -10,22 +8,8 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   imports: [RouterOutlet, NavigationComponent],
-  providers: [HttpClient, OpenLibraryBooksService, GoogleApiBooksService]
+  providers: [HttpClient]
 })
 
 export class AppComponent {
-
-  constructor(private booksService: OpenLibraryBooksService, private googleApiBooksService: GoogleApiBooksService) { }
-
-  ngOnInit() {
-    // this.booksService.findBookBySubject("Fiction").subscribe(data => {
-    //   console.log(data);
-    // });
-
-    // console.log('//////')
-
-    // this.googleApiBooksService.findBookBySubject("Fiction").subscribe(data => {
-    //   console.log(data);
-    // });
-  }
 }
