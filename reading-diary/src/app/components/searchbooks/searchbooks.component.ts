eb3e92b0826305cb53a775f117b860f120847b28
Maya Peneva <m.peneva@onebitsoftware.net>
2025-03-28 11:19:56 +0200

    Added searchbooks component.

---
+++ reading-diary/src/app/components/searchbooks/searchbooks.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-searchbooks',
+  imports: [RouterModule],
+  templateUrl: './searchbooks.component.html',
+  styleUrl: './searchbooks.component.css'
+})
+
+export class SearchbooksComponent {
+
+}
